2000db0297012c3b8d29640f39644bb713a88154
Ashwajit <thukral_ashwajit@yahoo.co.in>
2018-09-23 18:51:27 +0100

    Tests Class for POM Project

---
+++ Tests/src/test/java/com/pluralsight/UnitTest.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitTest {
+
+	@Test
+	public void canGoToHomePage() {
+		Pages.homepage().goTo();
+		Assert.assertTrue(Pages.homepage().isAt());
+	}
+
+	@Test
+	public void canGoToLoginPage() {
+		Pages.loginpage().goTo();
+		Assert.assertTrue(Pages.loginpage().isAt());
+	}
+
+	@Test
+	public void canGoToGetStarted() {
+		Pages.getstarted().goTo();
+		Assert.assertTrue(Pages.getstarted().isAt());
+	}
+
+	@After
+	public void cleanUp() {
+		Browser.close();
+	}
+
+}
