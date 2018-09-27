3e01647c32ad5fb5c04054ef8eabb734d86d2fb7
Ashwajit <thukral_ashwajit@yahoo.co.in>
2018-09-27 22:00:58 +0100

    Package name renamed

--- Tests/src/test/java/com/virginmoney/UnitTest.java
+++ Tests/src/test/java/com/virginmoney/UnitTest.java
@@ -1,8 +1,11 @@
-package com.pluralsight;
+package com.virginmoney;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import com.virginmoney.Browser;
+import com.virginmoney.Pages;
 
 public class UnitTest {
 
@@ -16,7 +19,18 @@ public class UnitTest {
 	public void canGoTo1YrFRCEISAPage() {
 		Pages.oneyrfrceisa().goTo();
 		Assert.assertTrue(Pages.oneyrfrceisa().isAt());
+	}
 
+	@Test
+	public void canGoTo2YrFRCEISAPage() {
+		Pages.twoyrfrceisa().goTo();
+		Assert.assertTrue(Pages.twoyrfrceisa().isAt());
+	}
+
+	@Test
+	public void canGoTo5YrFRCEISAPage() {
+		Pages.fiveyrfrceisa().goTo();
+		Assert.assertTrue(Pages.fiveyrfrceisa().isAt());
 	}
 
 	@AfterMethod
