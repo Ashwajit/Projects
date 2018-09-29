3714f3d0a7a864c94388a9416c20823d97e7b0af
Ashwajit <thukral_ashwajit@yahoo.co.in>
2018-09-29 16:55:18 +0100

    added config.properties file

--- TestFramework/src/main/java/com/virginmoney/Browser.java
+++ TestFramework/src/main/java/com/virginmoney/Browser.java
@@ -1,16 +1,42 @@
 package com.virginmoney;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browser {
-	static WebDriver driver = null;
 
-	public static void goTo(String url) {
-		System.setProperty("webdriver.chrome.driver", "C:/Appium/chromedriver.exe");
-		driver = new ChromeDriver();
+	public static WebDriver driver;
+	public static String browserName;
+	static Properties prop = new Properties();
+
+	public static void goTo(String url) throws IOException {
+
+		InputStream ip = new FileInputStream(
+				"C:\\Users\\ashwajit\\git\\Projects\\TestFramework\\src\\main\\java\\com\\virginmoney\\config.properties");
+		prop.load(ip);
+		browserName = prop.getProperty("browser");
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:/Appium/chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("Firefox")) {
+			System.setProperty("webdriver.gecko.driver", "C:/Appium/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(url);
+
 	}
 
 	public static String title() {
--- TestFramework/src/main/java/com/virginmoney/FasPage.java
+++ TestFramework/src/main/java/com/virginmoney/FasPage.java
@@ -1,12 +1,19 @@
 package com.virginmoney;
 
+import java.io.IOException;
+
 public class FasPage {
 
 	static String url = "https://uk.virginmoney.com/savings/find/results/online";
 	static String title = "Savings Range | Virgin Money UK";
 
 	public void goTo() {
-		Browser.goTo(url);
+		try {
+			Browser.goTo(url);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public boolean isAt() {
