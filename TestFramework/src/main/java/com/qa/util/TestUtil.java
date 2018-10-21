3c5631a1b578db2c4ef7241be1f7ad874200ae65
Ashwajit <thukral_ashwajit@yahoo.co.in>
2018-10-21 19:09:01 +0100

    Increased Page load

--- TestFramework/src/main/java/com/qa/util/TestUtil.java
+++ TestFramework/src/main/java/com/qa/util/TestUtil.java
@@ -2,7 +2,7 @@ package com.qa.util;
 
 public class TestUtil {
 
-	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long PAGE_LOAD_TIMEOUT = 25;
 	public static long IMPLICITLY_WAIT = 20;
 	public static String oneyr_frc_eisa_url = "https://uk.virginmoney.com/savings/products/1_year_fixed_rate_cash_e_isa_issue_347/";
 	public static String twoyr_frc_eisa_url = "https://uk.virginmoney.com/savings/products/2_year_fixed_rate_cash_e_isa_issue_348/";
